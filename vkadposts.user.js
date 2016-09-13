57ad9c62ed8c116560b25f07b9ce884176a266e8
Anton Yablokov <stsav012@mail.ru>
2016-09-13 21:56:45 +0300

    Update vkadposts.user.js

--- vkadposts.user.js
+++ vkadposts.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name			Remove ad posts @VK
-// @version			0.1
-// @description		removes ad posts from groups by keywords
-// @match			*://*.vk.com/*
-// @copyright		2016, StSav012
-// @author			StSav012
-// @namespace		vkap
+// @name            Remove ad posts @VK
+// @version         0.2
+// @description	    removes ad posts from groups by keywords
+// @match           *://*.vk.com/*
+// @copyright       2016, StSav012
+// @author          StSav012
+// @namespace       vkap
 // @run-at          document-end
 // ==/UserScript==
 
@@ -26,57 +26,33 @@ var actualCode = '(' + function() {
 		"1media-buyer.ru", "itunes.apple.com%2Fapp%2Fapple-store%2Fid695634432", "sale-stop.ru", "offersboard.ru",
 		"elementaree.ru", "start-mobile.net", ".hitnsale.ru"
 	];
-	var n, o=0;
+	var n;
 	function cleanAd()
 	{
 		var divs = document.querySelectorAll("div._post, div.feed_row");
 		//		var divs = document.querySelectorAll("div.post_content");
 		n = divs.length;
 		var i, j;
-		if(o<n)											// divs have been added
+		for(i = 0; i<n; ++i)						// we check it from the very beginning and to the end
 		{
-			for(i = 0; i<n-o; ++i)						// we don't know whether a new divs were added at the end or at the beginning
-			{											// here we look at the beginning
-				d = divs[i];
+			d = divs[i];
+			if(d.getAttribute('no_ad') != 'true')	// from https://greasyfork.org/ru/scripts/1978-vk-com-no-politic-feed/code
+			{										// does it worth checking the post?
 				for(j=0; j<keywords.length; ++j)
 				{
 					if(d.innerHTML.includes(keywords[j]))
 					{
+						//	d.parentNode.style.backgroundColor = "red"; // ← for debugging purposes
 						d.parentNode.removeChild(d);
 						break;
 					}
-				}
-			}
-			for(i = (o>=n-o)?o:(n-o); i<n; ++i)			// we don't know whether a new divs were added at the end or at the beginning
-			{											// and here we look at the tail or whatever has left
-				d = divs[i];
-				for(j=0; j<keywords.length; ++j)
-				{
-					if(d.innerHTML.includes(keywords[j]))
+					else
 					{
-						d.parentNode.removeChild(d);
-						break;
+						d.setAttribute('no_ad', 'true');
 					}
 				}
 			}
 		}
-		else if(o>=n)
-		{
-			for(i = 0; i<n; ++i)						// we assume the dom tree has completely changed, so we check it from the very beginning and to the end
-			{
-				d = divs[i];
-				for(j=0; j<keywords.length; ++j)
-				{
-					if(d.innerHTML.includes(keywords[j]))
-					{
-						//					d.parentNode.style.backgroundColor = "red";
-						d.parentNode.removeChild(d);
-						break;
-					}
-				}
-			}
-		}
-		o = n;
 	}
 	cleanAd();
 	document.querySelector('div#page_body').addEventListener('DOMNodeInserted', cleanAd);
