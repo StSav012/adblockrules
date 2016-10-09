8f7ad08e5414ef5bc8516b7909250060fcc5d241
Anton Yablokov <stsav012@mail.ru>
2016-10-09 19:51:27 +0300

    Update tpb.user.js
    Google has banned a site formerly led to the Pirate Bay. The sites have been revised.
    It turned out that Google bans fastpiratebay.co.uk, gameofbay.org, …, piratebays.co, …, thepirateshore.eu, ukpirate.click, and ukpirate.org. Has anyone an idea about how to deal with the ban?

--- tpb.user.js
+++ tpb.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
-// @name			Get rid of the Pirate Bay pop-under
-// @version			0.1
-// @description		disables the Pirate Bay pop-under
-// @match *://*.123bay.online
+// @name			  Get rid of the Pirate Bay pop-under
+// @version			0.2
+// @description	disables the Pirate Bay pop-under
+// @match *://*.123bay.pw
 // @match *://*.ahoy.one
-// @match *://*.bayproxy.link
 // @match *://*.fastpiratebay.co.uk
 // @match *://*.gameofbay.org
 // @match *://*.ikwilthepiratebay.org
@@ -12,12 +11,9 @@
 // @match *://*.pbp.rocks
 // @match *://*.pirate.trade
 // @match *://*.piratebay.click
-// @match *://*.piratebay.host
 // @match *://*.piratebay.red
-// @match *://*.piratebay.zelwa.re
 // @match *://*.piratebayblocked.com
 // @match *://*.piratebaymirror.eu
-// @match *://*.piratebayproxy.tf
 // @match *://*.piratebayproxy.tf
 // @match *://*.piratebays.co
 // @match *://*.piratebays.co.uk
@@ -34,7 +30,6 @@
 // @match *://*.thehiddenbay.xyz
 // @match *://*.thepirate.zone
 // @match *://*.thepiratebay-proxy.com
-// @match *://*.thepiratebay.lu
 // @match *://*.thepiratebay.org
 // @match *://*.thepiratebay.run
 // @match *://*.thepiratebay.tech
@@ -44,7 +39,7 @@
 // @match *://*.tpb.dashitz.com
 // @match *://*.tpb.patatje.eu
 // @match *://*.tpb.portalimg.com
-// @match *://*.tpb.proxyduck.net
+// @match *://*.tpb.proxyduck.co
 // @match *://*.tpbmirror.us
 // @match *://*.tpbunblocked.org
 // @match *://*.ukpirate.click
@@ -55,10 +50,10 @@
 // @match *://*.vicetorrent.com
 // @copyright		2016, StSav012
 // @author			StSav012
-// @namespace https://greasyfork.org/users/61331
+// @namespace   https://greasyfork.org/users/61331
 // ==/UserScript==
 
-var actualCode = "_wm={};";
+var actualCode = "_wm={}; window.adk_pdisp={};";
 var script = document.createElement('script');
 script.textContent = actualCode;
 (document.body||document.documentElement).appendChild(script);
