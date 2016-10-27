862d69ae36604a90eebb8d39bc789adc9efc46e9
Anton Yablokov <stsav012@mail.ru>
2016-10-27 09:05:05 +0300

    Update vkadposts.user.js

--- vkadposts.user.js
+++ vkadposts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Remove ad posts @VK
-// @version         0.3.20161026.2
+// @version         0.3.20161027.1
 // @description	    removes ad posts from feed and walls by keywords
 // @match           *://*.vk.com/*
 // @copyright       2016, StSav012
@@ -35,8 +35,8 @@ var actualCode = '(' + function() {
 		"/vkchydaku", "/brandclubkiiik", "/web_highlights_kurs",
 		"Центр образовательных технологий Advance",
 		"Начни играть тут", "Победители будут выбраны случайным образом", "победитель будет выбран случайным образом",
-		"NovaPizza.ru", "skypeteach.ru", "advance-club.ru", "sdelano.ru", "edgarkulikov.ru", "citystarwear.com", "befree-school.ru",
-		"bright-shopping.ru", ".sale-gooods.ru", "www.kopikot.ru",
+		"NovaPizza.ru", "skypeteach.ru", "english4now.com", "advance-club.ru", "sdelano.ru", "edgarkulikov.ru", "citystarwear.com",
+		"befree-school.ru", "bright-shopping.ru", ".sale-gooods.ru", "www.kopikot.ru",
 		"1media-buyer.ru", "itunes.apple.com%2Fapp%2Fapple-store%2Fid695634432", "sale-stop.ru", "offersboard.ru", "artskills.ru",
 		"elementaree.ru", "start-mobile.net", ".hitnsale.ru", "вконкурс.рф", "printbar.ru", "tracking.leaddealer.net", "envylab.ru",
 		"app_title_"	// that's to avoid ads from games
