c506ab6730ef6e79ac0266e44ddc5b1ed28fd8f9
Anton Yablokov <stsav012@mail.ru>
2017-11-25 19:54:42 +0300

    Create domruadpage.user.js

---
+++ domruadpage.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            Skip domru ad pages
+// @version         0.1.20171125.1
+// @description	    skips domru ad pages
+// @match           *://info.ertelecom.ru/*
+// @grant           none
+// @copyright       2017, StSav012
+// @author          StSav012
+// @namespace       http://info.ertelecom.ru/
+// @run-at          document-idle
+// ==/UserScript==
+
+"use strict";
+
+var actualCode = '(' + function() {
+	var x = document.querySelectorAll("a[class='header__close js-close']");
+  if (x.length == 1) {
+    window.location = x[0].href;
+  }
+  return;
+} + ')();';
+var script = document.createElement('script');
+script.textContent = actualCode;
+(document.body||document.documentElement).appendChild(script);
