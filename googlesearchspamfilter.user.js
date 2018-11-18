2c4272bd56c999a7dbf5e7ad13008fa57ba17b41
Anton Yablokov <stsav012@mail.ru>
2018-11-18 20:03:23 +0300

    Create googlesearchspamfilter.user.js

---
+++ googlesearchspamfilter.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name            Google Search Spam Filter
+// @name:ru         Фильтр спама в результатах поиска Google
+// @description	    removes blacklisted search results
+// @description:ru  скрывает ненужные результаты в выдаче
+// @namespace       https://www.google.com/search
+// @include         /^https?://(www\.)?google\.\w+//
+// @copyright       2018, StSav012
+// @author          StSav012
+// @grant           none
+// @version         0.1.20181118.1
+// @run-at          document-end
+// ==/UserScript==
+
+/* jshint esversion: 6 */
+/* jshint browser: true */
+
+"use strict";
+
+// blacklisted URLs
+var spamURLs = ['http://qaru.site/', 'https://stackoverrun.com/'];
+
+for (let u of spamURLs) {
+  var junk = document.querySelectorAll('div a[href^="' + u + '"]:not([class])');
+  for (let j of junk) {
+    var p = j.parentNode;
+    while (p.tagName == 'DIV' && !p.classList.contains('g')) {
+      p = p.parentNode;
+    }
+    console.log('found', p);
+    p.parentNode.removeChild(p);
+  }
+}
