d318c10d25afc08631e6c04ae8bee0b91953cace
Anton Yablokov <stsav012@gmail.com>
2019-04-25 12:59:40 +0300

    Update rp5adfix.user.js

--- rp5adfix.user.js
+++ rp5adfix.user.js
@@ -20,53 +20,112 @@
 // @author          StSav012
 // @homepageURL     https://github.com/StSav012/adblockrules/blob/master/rp5adfix.user.js
 // @downloadURL     https://github.com/StSav012/adblockrules/raw/master/rp5adfix.user.js
-// @version         10
+// @version         11
 // ==/UserScript==
 
 "use strict";
 
 var div, script;
 if (typeof unsafeWindow !== 'undefined') {
-    if (typeof unsafeWindow.sAdIdContainerBottom !== 'undefined') {
-        div = document.createElement('DIV');
-        div.id = unsafeWindow.sAdIdContainerBottom.substr(1);
-        (document.body||document.documentElement).appendChild(div);
+  if (typeof unsafeWindow.sAdIdContainerBottom !== 'undefined') {
+    div = document.createElement('DIV');
+    div.id = unsafeWindow.sAdIdContainerBottom.substr(1);
+    (document.body||document.documentElement).appendChild(div);
+  }
+  if (typeof unsafeWindow.sContainer !== 'undefined') {
+    div = document.createElement('DIV');
+    div.id = unsafeWindow.sContainer.substr(1);
+    (document.body||document.documentElement).appendChild(div);
+  }
+  if (typeof unsafeWindow.isAdBlocker !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function isAdBlocker() {return false;}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof unsafeWindow.isAdFilter !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function isAdFilter() {return false;}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof unsafeWindow.gofirst !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function gofirst() {}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof unsafeWindow.sABMessage !== 'undefined') {
+    var scripts = document.querySelectorAll('head > script:not([src])');
+    for (let s of scripts) {
+      if (s.textContent.indexOf(unsafeWindow.sABMessage) != -1) {
+        var w1 = s.textContent.split('[');
+        var maxLength = -1, maxLengthIndex = 0;
+        for (let w of w1) {
+          var i = w.lastIndexOf(']');
+          if (maxLength < i) {
+            maxLength = i;
+            maxLengthIndex = w1.indexOf(w);
+          }
+        }
+        console.log(maxLength, maxLengthIndex);
+        if (maxLengthIndex > 0) {
+          var w2 = w1[maxLengthIndex - 1].split(' ');
+          var fn = w2[w2.lastIndexOf('function') + 1].split('(')[0];
+          script = document.createElement('SCRIPT');
+          script.textContent = 'function ' + fn + '() {addCookie("is_adblock", false, 365);}';
+          (document.body||document.documentElement).appendChild(script);
+          console.log(fn);
+          s.textContent += script.textContent;
+        }
+      }
     }
-    if (typeof unsafeWindow.sContainer !== 'undefined') {
-        div = document.createElement('DIV');
-        div.id = unsafeWindow.sContainer.substr(1);
-        (document.body||document.documentElement).appendChild(div);
-    }
-    if (typeof unsafeWindow.isAdBlocker !== 'undefined') {
-        script = document.createElement('SCRIPT');
-        script.textContent = 'function isAdBlocker() {return false;}';
-        (document.body||document.documentElement).appendChild(script);
-    }
-    if (typeof unsafeWindow.isAdFilter !== 'undefined') {
-        script = document.createElement('SCRIPT');
-        script.textContent = 'function isAdFilter() {return false;}';
-        (document.body||document.documentElement).appendChild(script);
-    }
+  }
 }
 else {
-    if (typeof sAdIdContainerBottom !== 'undefined') {
-        div = document.createElement('DIV');
-        div.id = sAdIdContainerBottom.substr(1);
-        (document.body||document.documentElement).appendChild(div);
+  if (typeof sAdIdContainerBottom !== 'undefined') {
+    div = document.createElement('DIV');
+    div.id = sAdIdContainerBottom.substr(1);
+    (document.body||document.documentElement).appendChild(div);
+  }
+  if (typeof sContainer !== 'undefined') {
+    div = document.createElement('DIV');
+    div.id = sContainer.substr(1);
+    (document.body||document.documentElement).appendChild(div);
+  }
+  if (typeof isAdBlocker !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function isAdBlocker() {return false;}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof isAdFilter !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function isAdFilter() {return false;}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof gofirst !== 'undefined') {
+    script = document.createElement('SCRIPT');
+    script.textContent = 'function gofirst() {}';
+    (document.body||document.documentElement).appendChild(script);
+  }
+  if (typeof sABMessage !== 'undefined') {
+    var scripts = document.querySelectorAll('head > script:not([src])');
+    for (let s of scripts) {
+      if (s.textContent.indexOf(sABMessage) != -1) {
+        var w1 = s.textContent.split('[');
+        var maxLength = -1, maxLengthIndex = 0;
+        for (let w of w1) {
+          var i = w.lastIndexOf(']');
+          if (maxLength < i) {
+            maxLength = i;
+            maxLengthIndex = w1.indexOf(w);
+          }
+        }
+        if (maxLengthIndex > 0) {
+          var w2 = w1[maxLengthIndex - 1].split(' ');
+          var i = w2.lastIndexOf('function');
+          script = document.createElement('SCRIPT');
+          script.textContent = 'function ' + w2[i+1] + ' {addCookie("is_adblock", false, 365);}';
+          (document.body||document.documentElement).appendChild(script);
+        }
+      }
     }
-    if (typeof sContainer !== 'undefined') {
-        div = document.createElement('DIV');
-        div.id = sContainer.substr(1);
-        (document.body||document.documentElement).appendChild(div);
-    }
-    if (typeof isAdBlocker !== 'undefined') {
-        script = document.createElement('SCRIPT');
-        script.textContent = 'function isAdBlocker() {return false;}';
-        (document.body||document.documentElement).appendChild(script);
-    }
-    if (typeof isAdFilter !== 'undefined') {
-        script = document.createElement('SCRIPT');
-        script.textContent = 'function isAdFilter() {return false;}';
-        (document.body||document.documentElement).appendChild(script);
-    }
+  }
 }
